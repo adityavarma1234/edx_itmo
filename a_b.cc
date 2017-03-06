a8bc01dfdf59834694b6786de62cbe93b4b8be44
Aditya varma <aditya.varma@grabonrent.com>
2017-03-06 20:19:10 +0530

    solved 2 easy problems

---
+++ a_b.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  long long int a,b;
+  cin >> a >> b;
+  cout << a + b << endl;
+  return 0;    
+}
